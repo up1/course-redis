c818f7e5cbd0899c134760bf3b30ffc5bab1b997
up1 <somkiat.p@gmail.com>
2025-03-05 00:54:47 +0700

    Load data and embedding

---
+++ workshop/demo-redis/demo-vector/step_2.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import numpy as np
+from sentence_transformers import SentenceTransformer
+pd.set_option('display.max_colwidth', None)
+
+# Load the dataset from huggingface datasets :: https://huggingface.co/datasets/pythainlp/thai_food_v1.0
+df = pd.read_parquet("hf://datasets/pythainlp/thai_food_v1.0/data/train-00000-of-00001-e8b362f32bb3715c.parquet")
+print(df.shape)
+# show only name column
+print(df.head()["name"])
+
+# Embed the text using SentenceTransformer
+model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
+def texts_to_embeddings(texts):
+  return [np.array(embedding, dtype=np.float32).tobytes() for embedding in model.encode(texts, show_progress_bar=True)]
+
+
+df["text_embedding"] = texts_to_embeddings(df["name"].tolist())
+print(df.shape)
+print(df.head()["text_embedding"])
+     
